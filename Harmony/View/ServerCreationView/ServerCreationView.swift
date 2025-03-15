120b2ef593acf9ee599d0abaad07dc67b408f164
Forrest-Arnold <forkalarn@gmail.com>
2025-03-15 01:12:28 -0700

    making progress

---
+++ Harmony/View/ServerCreationView/ServerCreationView.swift
@@ -0,0 +1,18 @@
+//
+//  ServerCreationView.swift
+//  Harmony
+//
+//  Created by Forrest Kalani Arnold on 3/15/25.
+//
+
+import SwiftUI
+
+struct ServerCreationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ServerCreationView()
+}
